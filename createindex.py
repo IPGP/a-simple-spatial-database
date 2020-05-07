1d7ee70870fda0821006a87d9087d862db81a9e2
scpsc <arthur.github@gmail.com>
2020-05-07 16:36:42 +0200

    Add files via upload

---
+++ createindex.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import argparse
+import os
+import sys
+import json
+
+# createindex.py - Generate index.html for the Spatial database web interface
+# Copyright (C) 2020 Arthur Delorme
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+# (Contact: delorme@ipgp.fr)
+
+print('''createindex.py Copyright (C) 2020 Arthur Delorme
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute
+it under certain conditions.
+See the GNU General Public License for more details.''')
+
+parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='Generate index.html for the Spatial database web interface'
+    )
+parser.add_argument('--bing_maps_key', type=str,
+    help="your Bing Maps API key (OpenStreetMap will be used by default)")
+parser.add_argument('--title', type=str,
+    help="title of the web page (the <title> and <h1> HTML elements)")
+
+args = parser.parse_args()
+
+# Scan the data directory
+geojson_files = {}
+for f in os.listdir('data'):
+    if f.endswith('.geojson'):
+        if ' ' in f:
+            sys.exit('Error with "{}": whitespaces not allowed in the GeoJSON file names'.format(f))
+        with open('data/'+f) as geojson_f:
+            data = json.load(geojson_f)
+            type = data['features'][0]['properties']['type']
+            geometry_type = data['features'][0]['geometry']['type']
+        if geometry_type not in ['Point', 'Polygon']:
+            sys.exit('Error with "{}": geometry type is not supported (only Point and Polygon are)')
+        geojson_files[f] = {
+            'type': type,
+            'geometry_type': geometry_type,
+            'prefix': os.path.splitext(f)[0]
+        }
+
+# Load the color list
+with open('colors.txt') as f:
+    color_list = f.read().splitlines()
+
+# Create file
+with open('template_index.html') as f_in, open('index.html', 'w') as f_out:
+    for l in f_in:
+        if '<title>' in l and args.title:
+            f_out.write('    <title>{}</title>\n'.format(args.title))
+        elif 'leaflet-bing-layer' in l and args.bing_maps_key:
+            f_out.write('    {}\n'.format(l.split('--')[1].strip()))
+        elif '//bing_maps' in l and args.bing_maps_key:
+            if 'your_bing_maps_key' in l:
+                f_out.write("    key = '{}';\n".format(args.bing_maps_key))
+            else:
+                f_out.write(l.replace('//bing_maps', ''))
+        elif '//openstreetmap' in l and not args.bing_maps_key:
+            f_out.write(l.replace('//openstreetmap', ''))
+        elif '<!-- YOUR_DATA -->' in l: # HTML part
+            for f in geojson_files:
+                f_out.write('    <link rel="{}" type="application/json" href="data/{}">\n'.format(
+                        geojson_files[f]['type'],
+                        f
+                    ))
+        elif '// --> YOUR_DATA <--' in l: #JS part
+            prefix_list = []
+            for f in geojson_files:
+                prefix = geojson_files[f]['prefix']
+                prefix_list.append(prefix)
+                f_out.write('    var {} = {{}};\n'.format(prefix))
+            f_out.write('    var allLayers = [{}];\n'.format(
+                    ', '.join(prefix_list)
+                ))
+            for i, f in enumerate(geojson_files):
+                color = color_list[i]
+                if geojson_files[f]['geometry_type'] == 'Point':
+                    f_out.write("    addMarkerLayer('{}', {}, '{}');\n".format(
+                            geojson_files[f]['type'],
+                            geojson_files[f]['prefix'],
+                            color
+                        ))
+                else:
+                    f_out.write("    addPolygonLayer('{}', {}, '{}');\n".format(
+                            geojson_files[f]['type'],
+                            geojson_files[f]['prefix'],
+                            color
+                        ))
+        else:
+            f_out.write(l)
